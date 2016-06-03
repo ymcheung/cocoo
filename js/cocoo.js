390db7c06e66e8ab946f84a0136dbdd8f3724bef
Yuming Cheung <ymcheung.tw@gmail.com>
2016-06-04 03:39:57 +0800

    Basic layouts

--- js/cocoo.js
+++ js/cocoo.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+
+// Document Ready
+document.addEventListener('DOMContentLoaded', function() {
+
+if (document.documentElement.clientWidth > 640) 
+	{
+	
+	}
+	else 
+	{
+		
+	}
+
+});
